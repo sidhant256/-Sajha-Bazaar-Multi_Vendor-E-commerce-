9c55c4835286e3d88bbf4f06854b6163bc2edd7c
Sidhant <sidhantpandit256@gmail.com>
2026-07-13 16:22:54 +0545

    [ADDED]:: added Order_items_table migration file with foreignId('Order_id') & ('product_variant_id')..

---
+++ database/migrations/2026_07_13_061451_create_order_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_variant_id')->constrained()->restrictOnDelete();
+            $table->foreignId('vendor_id')->cosntrained()->cascadeOnDelete();
+            $table->unsignedInteger('quantity');
+            $table->decimal('unit_price', 10, 2);
+            $table->timestamps();
+
+            $table->index(['order_id', 'vendor_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
